bd122532be2a854edd6f894a6d12b07e59580d5a
Pavel Vyukov <vukov87@mail.ru>
2020-03-22 16:22:25 +0300

    HW 8 is done

---
+++ L8_my_JOIN.sql
@@ -0,0 +1,34 @@
+USE vk_ex;
+
+-- 2. Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей
+SELECT COUNT(l.user_id) AS total 
+FROM likes AS l
+	 JOIN profiles AS p
+       ON l.target_id = p.user_id
+     JOIN target_types AS t 
+       ON l.target_type_id = t.id 
+     WHERE t.name = 'users' AND 
+           l.target_id IN 
+             (SELECT * FROM (SELECT user_id FROM profiles ORDER BY birthday DESC LIMIT 10) AS tmp); 
+
+            
+ -- 3. Определить кто больше поставил лайков (всего) - мужчины или женщины?
+SELECT p.gender , COUNT(p.gender ) AS total
+ FROM likes AS l 
+      JOIN profiles AS p 
+      ON l.user_id = p.user_id
+GROUP BY p.gender
+ORDER BY total DESC LIMIT 1;
+
+-- 4. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети.
+-- чем меньше пользователь ставит лайков, тем менее он активен
+SELECT CONCAT (u.first_name, ' ', u.last_name ) AS user , COUNT(l.user_id ) AS total_u
+  FROM profiles AS p  
+  LEFT JOIN likes AS l 
+       ON p.user_id = l.user_id 
+  JOIN users AS u 
+  	   ON u.id=p.user_id 
+ GROUP BY p.user_id
+ ORDER BY total_u LIMIT 10
+ ;
+ 
